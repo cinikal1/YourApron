d728ad2cbdf448d0258c73d8cdb2abe0bccbdd3d
Carlos <berdealcarlos@gmail.com>
2026-07-02 21:20:21 -0500

    Delete generate-fonts.bat

--- generate-fonts.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo.
-echo  YourApron — Google Fonts Generator
-echo  ------------------------------------
-set /p KEY="Paste your Google Fonts API key: "
-node generate-fonts.js %KEY%
-pause
